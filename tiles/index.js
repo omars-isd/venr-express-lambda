2ef2a3c35abd6dc8f48685346967a5fd8a54ed6f
Omar Shaikh <shaikho@infinity-software.com>
2018-12-31 14:08:52 -0500

    stronger validation

--- tiles/index.js
+++ tiles/index.js
@@ -2,19 +2,23 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+  try {
+    const x = Number.parseInt(req.query["x"]);
+    const y = Number.parseInt(req.query["y"]);
+    const z = Number.parseInt(req.query["z"]);
 
-  const x = req.query["x"];
-  const y = req.query["y"];
-  const z = req.query["z"];
+    if (x == null || y == null || z == null) {
+      throw new Error("Invalid parameters. Set x, y, and z to valid values.");
+    }
 
-  if (x == null || y == null || z == null) {
-    res.status(500).send("Invalid parameters. Set x, y, and z.")
+    res.send({
+      "Output": `${x}, ${y}, ${z}`
+    });
+  }
+  catch (err) {
+    res.status(500).send(err)
     return;
   }
-
-  res.send({
-    "Output": `${x}, ${y}, ${z}`
-  });
 });
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
