71260b9da5061ca1bdb51f1c257e18064fc14db3
Omar Shaikh <shaikho@infinity-software.com>
2018-12-31 13:02:06 -0500

    change

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello Amazon!"
   });
 });
 
-app.post('/', function(req, res) {
+app.post('/', function (req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello Amazon!"
   });
 });
 
